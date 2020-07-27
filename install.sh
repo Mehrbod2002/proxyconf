5be91a2c092de0927587d8b3cddb2c630f001a5e
Amirhossein Akhlaghpour <51247512+Mehrbod2002@users.noreply.github.com>
2020-07-28 07:02:34 +1200

    Fix Error

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 sudo apt-get install proxychains
 sudo apt-get install python3-pip
 sudo pip3 install urllib
-echo "alias proxyconf='sudo python3 /proxyconf/p.py'" >> /$HOME/.bashrc
+echo "alias proxyconf='sudo python3 proxyconf/p.py'" >> /$HOME/.bashrc
